4514cff9c016df0b438555ab933b9b1ce5244fb3
yamaan93 <yamaan93@gmail.com>
2018-05-26 17:20:56 -0400

    started a new project, probably wont use

---
+++ isp/src/isp/Main.java
@@ -0,0 +1,16 @@
+package isp;
+
+import hsa_ufa.Console;
+
+public class Main {
+
+	public static void setupGUI() {
+		Console c = new Console(700, 700, "isp");
+
+	}
+
+	public static void main(String[] args) {
+		setupGUI();
+	}
+
+}
